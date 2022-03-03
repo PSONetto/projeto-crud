bb9627b296a3156fb055cc8abd7b51c222c134b7
Plínio Netto <plinio.ice.pns@gmail.com>
2022-03-02 21:14:53 -0300

    feat: implementa o menu de navegação da página

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -2,4 +2,15 @@ import './Nav.css'
 import React from 'react'
 
 export default props =>
-    <aside className="menu-area">Nav</aside>
+    <aside className="menu-area">
+        <nav className="menu">
+            {/* Refatorar => */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+            <a href=""></a>
+        </nav>
+    </aside>
