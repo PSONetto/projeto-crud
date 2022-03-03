466d7ddafbcdfe0952b6467b2b94604aa8f04810
Plínio Netto <plinio.ice.pns@gmail.com>
2022-03-02 21:05:59 -0300

    feat: implementa o footer da página

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Footer
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Cod</strong><span className="text-danger">3</span><strong>r</strong>
+        </span>
     </footer>
